b803889e1510865c4194728df74ace2c1d7409fc
imoto-y <imoto-y@sys.i.kyoto-u.ac.jp>
2020-06-08 19:21:55 +0900

    これはわからなかった。AC

--- abc046/d/code.cpp
+++ abc046/d/code.cpp
@@ -7,34 +7,14 @@ int main()
 {
     std::string str;
     std::cin >> str;
-    const int n = str.size();
-    std::vector<int> current(n, -n * 2), next(n, 0);
-    current[0] = 0;
-    for (const char c : str)
-    {
-        if (c == 'g')
-        {
-            next[0] = current[1] + 1;
-            for (int i = 1; i < n - 1; ++i)
-            {
-                next[i] = std::max(current[i + 1] + 1, current[i - 1]);
-            }
-            next[n - 1] = current[n - 2];
+    int count = 0;
+    for(const char c:str){
+        if(c=='p'){
+            ++count;
         }
-        else
-        {
-            // c == 'p'
-            next[0] = current[1];
-            for (int i = 1; i < n - 1; ++i)
-            {
-                next[i] = std::max(current[i + 1], current[i - 1] - 1);
-            }
-            next[n - 1] = current[n - 2] - 1;
-        }
-        current.swap(next);
     }
-    const auto it = std::max_element(current.begin(), current.end());
-    std::cout << *it << std::endl;
+    
+    std::cout << str.size() / 2 - count << std::endl;
 
     return 0;
 }

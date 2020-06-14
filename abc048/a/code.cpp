5914e5f066891f15d0fc820321c55f10639c2f97
imoto-y <imoto-y@sys.i.kyoto-u.ac.jp>
2020-06-15 08:26:53 +0900

    AC

---
+++ abc048/a/code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s1, s2, s3;
+    std::cin >> s1 >> s2 >> s3;
+    std::cout << 'A' << s2[0] << 'C' << std::endl;
+    return 0;
+}

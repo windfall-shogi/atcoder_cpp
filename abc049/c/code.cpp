9bddf883462ed0f64fda9f721b85e3a16a16d7e6
imoto-y <imoto-y@sys.i.kyoto-u.ac.jp>
2020-06-15 23:54:40 +0900

    AC

--- abc049/c/code.cpp
+++ abc049/c/code.cpp
@@ -1,33 +1,80 @@
 #include <iostream>
 #include <string>
-#include <regex>
 
 int main()
 {
     std::string s;
     std::cin >> s;
-
-    std::smatch smatch;
-    const std::regex r("(dream|dreamer|erase|eraser)$");
-    while (true)
+    bool flag = true;
+    for (std::string::const_reverse_iterator it = s.rbegin(); it != s.rend();)
     {
-        if (std::regex_search(s, smatch, r))
+        if (*it == 'm')
         {
-            for (auto it = smatch[0].first; it != smatch[0].second; ++it)
+            ++it;
+            if (!(std::distance(it, s.crend()) >= 4 && *it++ == 'a' && *it++ == 'e' && *it++ == 'r' && *it++ == 'd'))
             {
-                s.pop_back();
+                flag = false;
+                break;
             }
-            if (s.empty())
+        }
+        else if (*it == 'e')
+        {
+            ++it;
+            if (!(std::distance(it, s.crend()) >= 4 && *it++ == 's' && *it++ == 'a' && *it++ == 'r' && *it++ == 'e'))
             {
-                std::cout << "YES" << std::endl;
+                flag = false;
+                break;
+            }
+        }
+        else if (*it == 'r')
+        {
+            ++it;
+            if (!(it != s.rend() && *it++ == 'e'))
+            {
+                flag = false;
+                break;
+            }
+
+            if (*it == 'm')
+            {
+                ++it;
+                if (!(std::distance(it, s.crend()) >= 4 && *it++ == 'a' && *it++ == 'e' && *it++ == 'r' && *it++ == 'd'))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            else if (*it == 's')
+            {
+                ++it;
+                if (!(std::distance(it, s.crend()) >= 3 && *it++ == 'a' && *it++ == 'r' && *it++ == 'e'))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            else
+            {
+                flag = false;
                 break;
             }
         }
         else
         {
-            std::cout << "NO" << std::endl;
+            flag = false;
             break;
         }
     }
+
+    if (flag)
+    {
+        std::cout << "YES";
+    }
+    else
+    {
+        std::cout << "NO";
+    }
+    std::cout << std::endl;
+
     return 0;
 }

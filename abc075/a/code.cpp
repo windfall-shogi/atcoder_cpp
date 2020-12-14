773c1870f7641ecb827dd8a71b175550b09927b0
imoto-y <imoto-y@sys.i.kyoto-u.ac.jp>
2020-12-14 18:58:36 +0900

    XORでシンプルにAC

--- abc075/a/code.cpp
+++ abc075/a/code.cpp
@@ -4,18 +4,7 @@ int main()
 {
     int a, b, c;
     std::cin >> a >> b >> c;
-    if (a == b)
-    {
-        std::cout << c << std::endl;
-    }
-    else if (a == c)
-    {
-        std::cout << b << std::endl;
-    }
-    else
-    {
-        std::cout << a << std::endl;
-    }
+    std::cout << (a ^ b ^ c) << std::endl;
 
     return 0;
 }

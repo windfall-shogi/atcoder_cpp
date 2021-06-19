ce19d0efe8668f66de777b7a612cb3a833139d05
Imoto Yasuhiro <soyiharu@gmail.com>
2021-06-20 02:07:29 +0900

    UnionFindに自分と同じグループに属するの要素数を取得する機能を追加

--- template.cpp
+++ template.cpp
@@ -27,8 +27,9 @@ namespace std
 struct UnionFind
 {
     std::vector<int> parent; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
+    std::vector<int> count;
 
-    UnionFind(int n) : parent(n)
+    UnionFind(int n) : parent(n), count(n, 1)
     {
         //最初は全てが根であるとして初期化
         for (int i = 0; i < n; i++)
@@ -44,7 +45,14 @@ struct UnionFind
         {
             return x;
         }
-        return parent[x] = root(parent[x]);
+        const int p = root(parent[x]);
+        if (p != parent[x])
+        {
+            // まだ書き換えが済んでいない部分について木の根に要素数を移し替える
+            count[p] += count[parent[x]];
+            count[parent[x]] = 0;
+        }
+        return parent[x] = p;
     }
 
     // xとyの木を併合
@@ -53,7 +61,12 @@ struct UnionFind
         int rx = root(x); //xの根をrx
         int ry = root(y); //yの根をry
         if (rx == ry)
-            return;      //xとyの根が同じ(=同じ木にある)時はそのまま
+            return; //xとyの根が同じ(=同じ木にある)時はそのまま
+
+        // 要素数を移し替える
+        count[ry] += count[rx];
+        count[rx] = 0;
+
         parent[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
     }
 
@@ -63,6 +76,12 @@ struct UnionFind
         int rx = root(x);
         int ry = root(y);
         return rx == ry;
+    }
+
+    // xが属する木の要素数を返す
+    int getCount(int x)
+    {
+        return count[root(x)];
     }
 };
 

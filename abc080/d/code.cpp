e745db99e7736322c64eac7c172fc3c100a8fbf8
Imoto Yasuhiro <soyiharu@gmail.com>
2021-07-31 08:14:18 +0900

    不正解になるパターンに気付いたので修正

--- abc080/d/code.cpp
+++ abc080/d/code.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <utility>
+#include <iterator>
+#include <algorithm>
 
 struct Program
 {
@@ -12,7 +15,8 @@ struct CompareA
     bool operator()(const Program p1, const Program p2) const
     {
         // trueになるとp1はp2より後ろになる
-        if(p2.s == p1.s){
+        if (p2.s == p1.s)
+        {
             return p2.c < p1.c;
         }
         return p2.s < p1.s;
@@ -24,7 +28,8 @@ struct CompareB
     bool operator()(const Program p1, const Program p2) const
     {
         // trueになるとp1はp2より後ろになる
-        if(p2.t == p1.t){
+        if (p2.t == p1.t)
+        {
             return p2.c < p1.c;
         }
         return p2.t < p1.t;
@@ -36,13 +41,51 @@ int main()
     int n, c;
     std::cin >> n >> c;
 
-    std::priority_queue<Program, std::vector<Program>, CompareA> qa;
-    std::priority_queue<Program, std::vector<Program>, CompareB> qb;
+    std::vector<std::vector<std::pair<int, int>>> buffer(c);
     for (int i = 0; i < n; ++i)
     {
-        Program tmp;
-        std::cin >> tmp.s >> tmp.t >> tmp.c;
-        qa.push(tmp);
+        int s, t, channel;
+        std::cin >> s >> t >> channel;
+        buffer[channel - 1].emplace_back(s, t);
+    }
+
+    std::priority_queue<Program, std::vector<Program>, CompareA> qa;
+    std::priority_queue<Program, std::vector<Program>, CompareB> qb;
+
+    for (int i = 0; i < c; ++i)
+    {
+        auto &buf = buffer[i];
+        std::sort(buf.begin(), buf.end());
+        for (auto it1 = buf.begin(); it1 != buf.end(); ++it1)
+        {
+            if (it1->first < 0)
+            {
+                continue;
+            }
+            for (auto it2 = std::next(it1); it2 != buf.end(); ++it2)
+            {
+                if (it1->second == it2->first)
+                {
+                    it1->second = it2->second;
+
+                    it2->first = it2->second = -1;
+
+                    continue;
+                }
+                break;
+            }
+        }
+
+        for (const auto &e : buf)
+        {
+            if (e.first < 0)
+            {
+                continue;
+            }
+
+            Program p{e.first, e.second, i + 1};
+            qa.push(p);
+        }
     }
 
     size_t ans = 0;

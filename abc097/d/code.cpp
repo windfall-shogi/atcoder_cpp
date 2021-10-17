73d88ac88c7f5a8c26f82b6a25f1aae1b9620664
Imoto Yasuhiro <soyiharu@gmail.com>
2021-10-18 08:39:04 +0900

    AC

---
+++ abc097/d/code.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+struct UnionFind
+{
+    std::vector<int> parent; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
+    std::vector<int> count;
+
+    UnionFind(int n) : parent(n), count(n, 1)
+    {
+        //最初は全てが根であるとして初期化
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+    int root(int x)
+    {
+        if (parent[x] == x)
+        {
+            return x;
+        }
+        const int p = root(parent[x]);
+        if (p != parent[x])
+        {
+            // まだ書き換えが済んでいない部分について木の根に要素数を移し替える
+            count[p] += count[parent[x]];
+            count[parent[x]] = 0;
+        }
+        return parent[x] = p;
+    }
+
+    // xとyの木を併合
+    void unite(int x, int y)
+    {
+        int rx = root(x); //xの根をrx
+        int ry = root(y); //yの根をry
+        if (rx == ry)
+            return; //xとyの根が同じ(=同じ木にある)時はそのまま
+
+        // 要素数を移し替える
+        count[ry] += count[rx];
+        count[rx] = 0;
+
+        parent[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+    }
+
+    // 2つのデータx, yが属する木が同じならtrueを返す
+    bool same(int x, int y)
+    {
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+
+    // xが属する木の要素数を返す
+    int getCount(int x)
+    {
+        return count[root(x)];
+    }
+};
+
+int main()
+{
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> ps(n);
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> ps[i];
+        --ps[i];
+    }
+    UnionFind uf(n);
+    for (int i = 0; i < m; ++i)
+    {
+        int x, y;
+        std::cin >> x >> y;
+        --x;
+        --y;
+
+        uf.unite(x, y);
+    }
+
+    std::unordered_map<int, std::unordered_set<int>> index_map;
+    for (int i = 0; i < n; ++i)
+    {
+        const int root = uf.root(i);
+        index_map[root].emplace(i);
+    }
+
+    int ans = 0;
+    for (const auto &e : index_map)
+    {
+        const auto &map = e.second;
+        for (int i : map)
+        {
+            const int p = ps[i];
+            const auto it = map.find(p);
+            if (it != map.end())
+            {
+                ++ans;
+            }
+        }
+    }
+
+    std::cout << ans << std::endl;
+
+    return 0;
+}

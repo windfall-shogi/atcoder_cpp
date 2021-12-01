5420e36e6fc57bed52cee4dba30905619868919d
Imoto Yasuhiro <soyiharu@gmail.com>
2021-12-01 19:24:20 +0900

    解説のおかげでAC

---
+++ abc229/e/code.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+
+struct UnionFind
+{
+    std::vector<int> parent; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
+    std::vector<int> count;
+
+    UnionFind(int n) : parent(n), count(n, 1)
+    {
+        //最初は全てが根であるとして初期化
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+    int root(int x)
+    {
+        if (parent[x] == x)
+        {
+            return x;
+        }
+        const int p = root(parent[x]);
+        if (p != parent[x])
+        {
+            // まだ書き換えが済んでいない部分について木の根に要素数を移し替える
+            count[p] += count[parent[x]];
+            count[parent[x]] = 0;
+        }
+        return parent[x] = p;
+    }
+
+    // xとyの木を併合
+    void unite(int x, int y)
+    {
+        int rx = root(x); //xの根をrx
+        int ry = root(y); //yの根をry
+        if (rx == ry)
+            return; //xとyの根が同じ(=同じ木にある)時はそのまま
+
+        // 要素数を移し替える
+        count[ry] += count[rx];
+        count[rx] = 0;
+
+        parent[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+    }
+
+    // 2つのデータx, yが属する木が同じならtrueを返す
+    bool same(int x, int y)
+    {
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+
+    // xが属する木の要素数を返す
+    int getCount(int x)
+    {
+        return count[root(x)];
+    }
+};
+
+int main()
+{
+    int N, M;
+    std::cin >> N >> M;
+    std::vector<std::vector<int>> edges(N);
+    for (int i = 0; i < M; ++i)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        --a;
+        --b;
+        edges[a].emplace_back(b);
+        // nodes[b].emplace_back(a);
+    }
+
+    UnionFind uf(N);
+    std::vector<int> ans;
+    ans.reserve(N);
+    ans.emplace_back(0);
+
+    int count = 0;
+    // ノード番号がN-1から1までのノードを順に追加する
+    for (int i = N - 1; i >= 1; --i)
+    {
+        ++count;
+        for (const auto j : edges[i])
+        {
+            if (!uf.same(i, j))
+            {
+                // ノード番号jとはまだ繋がっていないので、繋ぐ
+                uf.unite(i, j);
+                --count;
+            }
+        }
+        ans.emplace_back(count);
+    }
+
+    for (auto it = ans.rbegin(); it != ans.rend(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    return 0;
+}

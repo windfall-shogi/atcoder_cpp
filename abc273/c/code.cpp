f4cc8361abda56d20975677131d78011a598c3d9
Imoto Yasuhiro <soyiharu@gmail.com>
2022-10-16 00:10:24 +0900

    解き方がかなりナイーブだったのでやり直し

--- abc273/c/code.cpp
+++ abc273/c/code.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 int main()
 {
     int n;
     std::cin >> n;
-    std::vector<int64_t> as(n), bs(n), cs(n);
+    std::vector<int64_t> as(n), bs(n);
+    std::unordered_map<int64_t, int> count;
     for (int i = 0; i < n; ++i)
     {
         std::cin >> as[i];
         bs[i] = as[i];
+        ++count[as[i]];
     }
     std::sort(bs.begin(), bs.end());
     bs.erase(std::unique(bs.begin(), bs.end()), bs.end());
 
     for (int i = 0; i < n; ++i)
     {
-        const auto it = std::upper_bound(bs.begin(), bs.end(), as[i]);
-        const auto d = std::distance(it, bs.end());
-        ++cs[d];
-    }
-
-    for (const auto c : cs)
-    {
-        std::cout << c << std::endl;
+        if (i >= bs.size())
+        {
+            std::cout << 0 << std::endl;
+        }
+        else
+        {
+            std::cout << count.at(bs[bs.size() - 1 - i]) << std::endl;
+        }
     }
 
     return 0;
